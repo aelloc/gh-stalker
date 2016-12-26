8d5e896f87a64fc605a0bbd71f73e30b9a047e25
Calvin Gerling <calvingerling@outlook.com>
2016-12-26 15:56:50 -0200

    refactor(user-list): use $user to map the user response

--- components/user-list.js
+++ components/user-list.js
@@ -27,28 +27,13 @@
             getUser: function(e) {
                 let self = this;
                 github.user(e.target.innerText).then(response => {
-                    store.commit('bindUser', self.mapUser(response.data));
+                    store.commit('bindUser', $user.map(response.data));
                     store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, userSelected: true });
                     store.dispatch('searchRepositories');
                 }, response => {
                     store.commit('increaseCount');
                     store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, message: response.data.message, userSelected: true });
                 });
-            },
-            mapUser: function(user) {
-               let dateFormat = "DD/MM/YYYY hh:mm:ss";
-               return {
-                   login: user.login,
-                   avatar_url: user.avatar_url,
-                   html_url: user.html_url,
-                   name: user.name,
-                   bio: user.bio,
-                   followers: user.followers,
-                   following: user.following,
-                   created_at: moment(user.created_at).format(dateFormat),
-                   updated_at: moment(user.updated_at).format(dateFormat),
-                   repositories: []
-               };
             }
         }
     });
