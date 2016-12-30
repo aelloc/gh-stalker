0c8939a0e2a2bb1cfd803c1c022dede54e3b8030
Aelloc <aelloc@singlebit.org>
2016-12-30 11:49:53 -0200

    feat(repository): add single-repository list item component

---
+++ components/repository.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function() {
+    Vue.component('repository', {
+        props: ['repository'],
+        template: `<a v-on:click="commits" class="panel-block" :data-branch="repository.default_branch">
+                        <span class="panel-icon">
+                            <i v-if="repository.fork" class="fa fa-code-fork"></i>
+                            <i v-else class="fa fa-book"></i>
+                        </span>
+                        <p>{{repository.name}}</p>
+                    </a>`,
+        computed: {
+            user: () => store.state.user.login
+        },
+        methods: {
+            commits: function(event) {
+                let self = this;
+                let repository = event.target.innerText.trim();
+                $github.commits(self.user, repository).then(response => {
+                    store.commit('bindCommits', { commits: response.data, repository: repository });
+                    store.dispatch('getBranchs', repository);
+                });
+            }
+        }
+    });
+})();
