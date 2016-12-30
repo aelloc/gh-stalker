61dd7d96a2c0e3a631ffbf64b79d68821960fbe3
Calvin Gerling <calvingerling@outlook.com>
2016-12-30 16:14:48 -0200

    refactor(commits): change reference name to the commits state

--- components/commits.js
+++ components/commits.js
@@ -6,7 +6,7 @@
                         <p class="control">
                             <span class="select">
                                 <select v-on:change="changeBranch" v-model="actual_branch">calvingerling
-                                    <option v-for="branch in commits.branchs" :value="branch.name">{{branch.name}}</option>
+                                    <option v-for="branch in commit.branchs" :value="branch.name">{{branch.name}}</option>
                                 </select>
                             </span>
                         </p>
@@ -14,17 +14,17 @@
                     </div>`,
         computed: {
             commits: () => store.state.commits,
-            default_branch: () => store.state.user.repositories.filter(repository => repository.name == store.state.commits.repository)[0].default_branch
+            default_branch: () => store.state.user.repositories.filter(repository => repository.name == store.state.commit.repository)[0].default_branch
         },
         data: () => {
             return {
-                actual_branch: store.state.user.repositories.filter(repository => repository.name == store.state.commits.repository)[0].default_branch
+                actual_branch: store.state.user.repositories.filter(repository => repository.name == store.state.commit.repository)[0].default_branch
             }
         },
         methods: {
             changeBranch: function(event) {
                 this.actual_branch = event.target.value;
-                $github.commitsAt(store.state.user.login, store.state.commits.repository, event.target.value).then(response => {
+                $github.commitsAt(store.state.user.user.login, store.state.commits.repository, event.target.value).then(response => {
                     store.commit('bindCommits', { commits: response.data, repository: store.state.commits.repository});
                 });
             }
