7fab97e1ff2fe644edb758a6ec513e74912aba64
Calvin Gerling <calvingerling@outlook.com>
2016-12-30 11:47:12 -0200

    refactor(comments): change name to comments and use the new single-coment component

---
+++ components/comments.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function() {
+    Vue.component('comments', {
+        template: `<div>
+                        <div v-for="c in comments">
+                            <comment :commentary="c"></comment>
+                        </div>
+                    </div>`,
+            computed: {
+                commit_sha: () => store.state.commits.comments.sha,
+                comments: () => store.state.commits.comments.comments
+            }
+    });
+})();
