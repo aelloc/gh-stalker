c07eb62cd1cc2340dd30eaeebe0bb5346969fa23
Calvin Gerling <calvingerling@outlook.com>
2017-01-23 11:13:30 -0200

    refactor(actions): declare methods with es2015 short-hand

--- store/actions.js
+++ store/actions.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const actions = {
-    searchRepositories: () => {
+    searchRepositories () {
         if(!store.state.user.user.login)
             return;
 
@@ -11,14 +11,14 @@ const actions = {
             store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, message: response.body.message });
         });
     },
-    getBranchs: (store, repository) => {
+    getBranchs (store, repository = {}) {
         $github.branchs(store.state.user.user.login, repository).then(response => {
             store.commit('bindBranchs', response.data);
         }).catch(response => {
             store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, message: response.body.message });
         });
     },
-    getCommitComments: (store, sha) => {
+    getCommitComments (store, sha = '') {
         $github.commitComments(store.state.user.user.login, store.state.commit.repository, sha).then(response => {
             store.commit('bindComments', { sha: sha, comments: response.data });
         }).catch(response => {
