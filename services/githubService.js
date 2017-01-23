557bb40789625230c3585dc1a884db8644f7789f
Aelloc <aelloc@singlebit.org>
2017-01-23 10:54:59 -0200

    refactor(GitHubService): add default value on methods parameters

--- services/githubService.js
+++ services/githubService.js
@@ -5,31 +5,31 @@ class GitHubService {
         this.unk_avatar = unk_avatar;
     }
 
-    branchs(owner, repository) {
+    branchs(owner = '', repository = '') {
         return axios.get(`${this._url}/repos/${owner}/${repository}/branches`);
     }
 
-    commits(owner, repository) {
+    commits(owner = '', repository = '') {
         return axios.get(`${this._url}/repos/${owner}/${repository}/commits`);
     }
 
-    commitsAt(owner, repository, branch) {
+    commitsAt(owner = '', repository = '', branch = '') {
         return axios.get(`${this._url}/repos/${owner}/${repository}/commits?sha=${branch}`);
     }
 
-    searchUser(user) {
+    searchUser(user = '') {
         return axios.get(`${this._url}/search/users?utf8=%E2%9C%93&q=${user.replace(" ", "+")}`);
     }
 
-    searchRepositories(user) {
+    searchRepositories(user = '') {
         return axios.get(`${this._url}/users/${user}/repos`);
     }
 
-    user(user) {
+    user(user = '') {
         return axios.get(`${this._url}/users/${user}`);
     }
 
-    commitComments(owner, repo, ref) {
+    commitComments(owner = '', repo = '', ref = '') {
         return axios.get(`${this._url}/repos/${owner}/${repo}/commits/${ref}/comments`);
     }
 }
