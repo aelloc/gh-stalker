c569f4d1f5845ebd4dc3963e4ade2da8525044d3
Aelloc <aelloc@singlebit.org>
2017-01-23 11:38:33 -0200

    refactor(actions): use mutation-types in the store actions

--- store/actions.js
+++ store/actions.js
@@ -6,23 +6,23 @@ const actions = {
             return;
 
         $github.searchRepositories(store.state.user.user.login).then(response => {
-            store.commit('bindRepositories', response.data);
+            store.commit(types.UPDATE.REPOS, response.data);
         }).catch(response => {
-            store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, message: response.body.message });
+            store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
         });
     },
     getBranchs (store, repository = {}) {
         $github.branchs(store.state.user.user.login, repository).then(response => {
-            store.commit('bindBranchs', response.data);
+            store.commit(types.UPDATE.BRANCHS, response.data);
         }).catch(response => {
-            store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, message: response.body.message });
+            store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
         });
     },
     getCommitComments (store, sha = '') {
         $github.commitComments(store.state.user.user.login, store.state.commit.repository, sha).then(response => {
-            store.commit('bindComments', { sha: sha, comments: response.data });
+            store.commit(types.UPDATE.COMMENTS, { sha: sha, comments: response.data });
         }).catch(response => {
-            store.commit('updatePage', { isOk: response.status == 200, status: response.statusText, message: response.body.message });
+            store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
         });
     }
 };
