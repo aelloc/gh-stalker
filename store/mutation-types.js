dc8c5fb9fea4adad5946387747e48a6045d09a51
Calvin Gerling <calvingerling@outlook.com>
2017-01-23 11:15:04 -0200

    feat(mutation-types): add map object with the name of mutations

---
+++ store/mutation-types.js
@@ -0,0 +1,13 @@
+const types = {
+    UPDATE: {
+        PAGE: 'updatePage',
+        SEARCH_RESULT: 'updateSearchResult',
+        USER: 'updateUser',
+        REPOS: 'updateRepos',
+        COMMITS: 'updateCommits',
+        BRANCHS: 'updateBranchs',
+        COMMENTS: 'updateComments'
+    },
+    INCREASE_SEARCH_COUNT: 'increaseCount',
+    PAGE_IS_LOADING: 'isLoading'
+};
