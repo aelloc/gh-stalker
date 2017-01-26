b02b644bb7ad4d3c1e48f06ea7d50da62f7d4cc8
Aelloc <aelloc@singlebit.org>
2017-01-26 16:25:56 -0200

    refactor(store): import the store modules, configure the Vue to use Vuex and export the store

--- store/index.js
+++ store/index.js
@@ -1,15 +1,22 @@
-'use strict';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import commit from './modules/commit';
+import page from './modules/page';
+import search from './modules/search';
+import user from './modules/user';
+import * as actions from './actions';
+import * as getters from './getters';
 
-const store = (function() {
-    return new Vuex.Store({
-        actions,
-        getters,
-        strict: true,
-        modules: {
-            page,
-            search,
-            user,
-            commit
-        }
-    });
-})();
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    actions,
+    getters,
+    strict: true,
+    modules: {
+        page,
+        search,
+        user,
+        commit
+    }
+});
