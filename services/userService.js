d2e90b1af32f60142872b2aafc2b3066e966d2d6
Aelloc <aelloc@singlebit.org>
2017-01-26 16:12:21 -0200

    refactor(userService): change class by individual modules

--- services/userService.js
+++ services/userService.js
@@ -1,49 +1,45 @@
-class UserService {
-    constructor(unk_avatar) {
-        this._unk_avatar = unk_avatar;
-    }
+const default_unk_avatar = 'https://i2.wp.com/assets-cdn.github.com/images/gravatars/gravatar-user-420.png';
 
-    asUser(user) {
-        return {
-            login: user.login,
-            avatar_url: user.avatar_url,
-            html_url: user.html_url,
-            name: user.name,
-            bio: user.bio,
-            type: user.type,
-            followers: user.followers,
-            following: user.following,
-            created_at: user.created_at,
-            updated_at: user.updated_at,
-            repositories: []
-        };
-    }
+export const asUser = function (user) {
+    return {
+        login: user.login,
+        avatar_url: user.avatar_url,
+        html_url: user.html_url,
+        name: user.name,
+        bio: user.bio,
+        type: user.type,
+        followers: user.followers,
+        following: user.following,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+        repositories: []
+    };
+};
 
-    asOrganization(org) {
-        return {
-            login: org.login,
-            avatar_url: org.avatar_url,
-            html_url: org.html_url,
-            name: org.name,
-            blog: org.blog,
-            bio: org.bio,
-            type: org.type,
-            created_at: org.created_at,
-            updated_at: org.updated_at,
-            public_repos: org.public_repos,
-            repositories: []
-        };
-    }
+export const asOrganization = function (org) {
+    return {
+        login: org.login,
+        avatar_url: org.avatar_url,
+        html_url: org.html_url,
+        name: org.name,
+        blog: org.blog,
+        bio: org.bio,
+        type: org.type,
+        created_at: org.created_at,
+        updated_at: org.updated_at,
+        public_repos: org.public_repos,
+        repositories: []
+    };
+};
 
-    asCommitter({ committer, innerCommitter }) {
-        committer = committer || {};
-        return {
-            avatar_url: committer.avatar_url || this._unk_avatar,
-            html_url: committer.html_url || '',
-            login: committer.login || '',
-            type: committer.type || 'User',
-            name: innerCommitter.name,
-            email: innerCommitter.email
-        };
-    }
-}
+export const asCommitter = function ({ committer, innerCommitter }) {
+    committer = committer || {};
+    return {
+        avatar_url: committer.avatar_url || default_unk_avatar,
+        html_url: committer.html_url || '',
+        login: committer.login || '',
+        type: committer.type || 'User',
+        name: innerCommitter.name,
+        email: innerCommitter.email
+    };
+};
