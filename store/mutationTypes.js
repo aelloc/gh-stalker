b603237e4c3346e411fd00b4a6ab1fd4eb2af317
Aelloc <aelloc@singlebit.org>
2017-01-26 16:15:53 -0200

    refactor(mutationTypes): change structure to simulate a enum

---
+++ store/mutationTypes.js
@@ -0,0 +1,11 @@
+export const UPDATE = {
+    PAGE: 'updatePage',
+    SEARCH_RESULT: 'updateSearchResult',
+    USER: 'updateUser',
+    REPOS: 'updateRepos',
+    COMMITS: 'updateCommits',
+    BRANCHS: 'updateBranchs',
+    COMMENTS: 'updateComments'
+};
+export const INCREASE_SEARCH_COUNT = 'increaseCount';
+export const PAGE_IS_LOADING = 'isLoading';
