f099c4783067571682e1022956870cc657dcf83d
Aelloc <aelloc@singlebit.org>
2017-01-26 16:08:54 -0200

    chore(webpack.config): setup the webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            }
+        ]
+    },
+    vue: {
+        loaders: {
+            js: 'babel'
+        }
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    }
+}
