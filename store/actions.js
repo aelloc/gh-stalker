58e12cd434b2f52aed703c3d8c67bd301a439bb5
Aelloc <aelloc@singlebit.org>
2017-01-26 16:20:25 -0200

    refactor(actions): add imports and change the structure to easy exportation

--- store/actions.js
+++ store/actions.js
@@ -1,28 +1,30 @@
-'use strict';
+import { searchRepos, branchs, commitComments } from '../services/githubService';
+import * as types from './mutationTypes';
 
-const actions = {
-    searchRepositories () {
-        if(!store.state.user.user.login)
-            return;
+export const searchRepositories = function (store) {
+    let userLogin = store.state.user.user.login;
+    if (!userLogin)
+        return;
 
-        $github.searchRepositories(store.state.user.user.login).then(response => {
-            store.commit(types.UPDATE.REPOS, response.data);
-        }).catch(response => {
-            store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
-        });
-    },
-    getBranchs (store, repository = '') {
-        $github.branchs(store.state.user.user.login, repository).then(response => {
-            store.commit(types.UPDATE.BRANCHS, response.data);
-        }).catch(response => {
-            store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
-        });
-    },
-    getCommitComments (store, sha = '') {
-        $github.commitComments(store.state.user.user.login, store.state.commit.repository, sha).then(response => {
-            store.commit(types.UPDATE.COMMENTS, { sha: sha, comments: response.data });
-        }).catch(response => {
-            store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
-        });
-    }
+    searchRepos(userLogin).then(response => {
+        store.commit(types.UPDATE.REPOS, response.data);
+    }).catch(response => {
+        store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
+    });
+};
+
+export const getBranchs = function (store, repository) {
+    branchs(store.state.user.user.login, repository).then(response => {
+        store.commit(types.UPDATE.BRANCHS, response.data);
+    }).catch(response => {
+        store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
+    });
+};
+
+export const getCommitComments = function (store, sha) {
+    commitComments(store.state.user.user.login, store.state.commit.repository, sha).then(response => {
+        store.commit(types.UPDATE.COMMENTS, { sha: sha, comments: response.data });
+    }).catch(response => {
+        store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText, message: response.body.message });
+    });
 };
