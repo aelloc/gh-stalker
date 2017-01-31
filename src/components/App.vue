5bb3852a835e0dc06b8ee928b14b7ddae97f334a
Aelloc <aelloc@singlebit.org>
2017-01-31 14:43:50 -0200

    refactor(App): remove users list and add router-view

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,25 +1,19 @@
 <template>
     <div id="app">
         <hero></hero>
-
         <div v-if="searchCount > 0 && !page.isOk">
             <error></error>
         </div>
-        <div v-if="page.isOk">
-            <div v-if="searchCount > 0 && !page.userSelected">
-                <users></users>
-            </div>
-        </div>
+        <router-view></router-view>
     </div>
 </template>
 <script>
     import { mapState } from 'vuex'
     import Hero from './Hero.vue';
     import Error from './Error.vue';
-    import Users from './Users.vue';
 
     export default {
-        components: { Hero, Error, Users },
+        components: { Hero, Error },
         computed: mapState({
             page: state => state.page,
             searchCount: state => state.search.count,
