d606535a44231b73cba20008a3c4980dc13f6728
Aelloc <aelloc@singlebit.org>
2017-01-31 15:33:30 -0200

    refactor(Hero): add href to nav-center item and link to search

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -9,9 +9,14 @@
                         </a>
                     </div>
                     <div class="nav-center">
-                        <a class="nav-item is-active">GH Stalker</a>
+                        <a class="nav-item is-active" href="/">GH Stalker</a>
                     </div>
                     <div class="nav-right">
+                        <a class="nav-item" href="/#/search">
+                            <span class="icon">
+                                <i class="fa fa-search"></i>
+                            </span>
+                        </a>
                         <a class="nav-item" target="_blank" href="//github.com">
                             <span class="icon">
                                 <i class="fa fa-github"></i>
