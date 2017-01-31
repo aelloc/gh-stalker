4928f8813c9bf6f05e0b766732154fbee9b5d5c6
Aelloc <aelloc@singlebit.org>
2017-01-31 14:18:05 -0200

    refactor(App): change header to Hero component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,27 +1,6 @@
 <template>
     <div id="app">
-        <section class="hero is-dark">
-            <div class="hero-head">
-                <header class="nav">
-                    <div class="container">
-                        <div class="nav-left">
-                            <a class="nav-item" target="_blank" href="http://github.com">
-                                <span class="icon">
-                                <i class="fa fa-clock"></i>
-                            </span>
-                            </a>
-                        </div>
-                    </div>
-                </header>
-            </div>
-
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title is-1 modeka">GH STALKER</h1>
-                    <search></search>
-                </div>
-            </div>
-        </section>
+        <hero></hero>
 
         <div v-if="searchCount > 0 && !page.isOk">
             <error></error>
@@ -35,12 +14,12 @@
 </template>
 <script>
     import { mapState } from 'vuex'
-    import Search from './Search.vue';
+    import Hero from './Hero.vue';
     import Error from './Error.vue';
     import Users from './Users.vue';
 
     export default {
-        components: { Search, Error, Users },
+        components: { Hero, Error, Users },
         computed: mapState({
             page: state => state.page,
             searchCount: state => state.search.count,
