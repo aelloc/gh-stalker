18a9083ae0794ed966b79b26dc95e36a1fe8cbc3
Aelloc <aelloc@singlebit.org>
2017-01-31 15:38:57 -0200

    refactor(Search): use component state to storage the result and add the users component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,36 +1,51 @@
 <template>
     <div>
-        <label class="label">
-            <a>
-                <i class="fa fa-user"></i>
-            </a>
-        </label>
-        <form v-on:submit="search">
-            <p class="control has-addons has-addons-centered">
-                <input v-model="search.term" class="input" type="text" placeholder="Find a user">
-                <button type="submit" class="button is-info" :class="{'is-loading': isLoading}">Search</a>
-            </p>
-        </form>
+        <section class="section">
+            <div class="container">
+                <form v-on:submit="search">
+                    <p class="control has-addons has-addons-centered">
+                        <input v-model="search.term" class="input" type="text" placeholder="Find a user">
+                        <button type="submit" class="button is-info" :class="{'is-loading': isLoading}">Search</a>
+                    </p>
+                </form>    
+            </div>
+        </section>
+        <div v-if="searchCount > 0">
+            <users title="Search result" :users="result"></users>
+        </div>
     </div>
 </template>
 <script>
-    import { mapGetters } from 'vuex';
+    import { mapGetters, mapState } from 'vuex';
     import { searchUser } from '../services/githubService';
+    import Users from './Users.vue';
     import * as types from '../store/mutationTypes';
 
     export default {
-        computed: mapGetters({
-            isLoading: 'page_loading'    
-        }),
+        data() {
+            return {
+                result: []
+            };
+        },
+        components: { Users },
+        computed: {
+            ...mapGetters({
+                isLoading: 'page_loading'
+            }),
+            ...mapState({
+                searchCount: state => state.search.count
+            })
+        },
         methods: {
             search(e) {
                 e.preventDefault();
-
+                
                 let self = this;
                 self.$store.commit(types.PAGE_IS_LOADING);
-                
+
                 searchUser(self.search.term).then(response => {
-                    self.$store.commit(types.UPDATE.SEARCH_RESULT, response.data);
+                    self.$data.result = response.data.items;
+
                     self.$store.commit(types.INCREASE_SEARCH_COUNT);
                     self.$store.commit(types.UPDATE.PAGE, { isOk: response.status == 200, status: response.statusText });
                 }, response => {
