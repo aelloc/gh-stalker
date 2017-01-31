53abc4485b555e1fe25fe9ecdccf5ae06d84d7cf
Aelloc <aelloc@singlebit.org>
2017-01-31 15:39:50 -0200

    refactor(router): change the component of path to Search

--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Profile from '../components/Profile.vue';
+import Search from '../components/Search.vue';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     base: '/',
     routes: [
+        { path: '/search', component: Search },
         { path: '/profile/:user', component: Profile }
     ]
 });
