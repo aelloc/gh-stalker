2b4bc6ec7b0a673ab1d0db735243f5a3f8206c1e
Aelloc <aelloc@singlebit.org>
2017-01-31 18:42:26 -0200

    feat(Hero): show navbar with options when viewed from mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,32 +1,57 @@
 <template>
-    <section class="hero is-dark is-medium">
-        <div class="hero-head">
-            <header class="nav">
-                <div class="container">
-                    <div class="nav-left">
-                        <a class="nav-item" href="/">
-                            <img src="../assets/images/logo.png">
-                        </a>
-                    </div>
-                    <div class="nav-center">
-                        <a class="nav-item is-active" href="/">GH Stalker</a>
-                    </div>
-                    <div class="nav-right">
-                        <a class="nav-item" href="/#/search">
-                            <span class="icon">
+    <div>
+        <section class="hero is-dark is-medium">
+            <header class="hero-head">
+                <nav class="nav">
+                    <div class="container">
+                        <div class="nav-left">
+                            <a class="nav-item" href="/">
+                                <img src="../assets/images/logo.png">
+                            </a>
+                        </div>
+                        <div class="nav-center">
+                            <a class="nav-item is-active" href="/">GH Stalker</a>
+                        </div>
+                        <div class="nav-right nav-menu">
+                            <a class="nav-item" href="/#/search">
+                                <span class="icon">
                                 <i class="fa fa-search"></i>
                             </span>
-                        </a>
-                        <a class="nav-item" target="_blank" href="//github.com">
-                            <span class="icon">
+                            </a>
+                            <a class="nav-item" target="_blank" href="//github.com">
+                                <span class="icon">
                                 <i class="fa fa-github"></i>
                             </span>
-                        </a>
+                            </a>
+                        </div>
                     </div>
-                </div>
+                </nav>
             </header>
-        </div>
-    </section>
+        </section>
+        <section class="hero is-info">
+            <header class="hero-head">
+                <nav class="nav">
+                    <div class="container">
+                        <div class="nav-center">
+                            <a class="nav-item" href="/#/search">
+                                <span class="icon"><i class="fa fa-search"></i></span>Search
+                            </a>
+                            <a class="nav-item" target="_blank" href="//github.com">
+                                <span class="icon"><i class="fa fa-github"></i></span>GitHub
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </section>
+    </div>
 </template>
 <script></script>
-<style></style>
+<style>
+    @media (min-width: 768px) {
+        section.hero.is-info {
+            display: none;
+        }
+    }
+    
+</style>
