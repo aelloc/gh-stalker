e4d353da014e092119c57e9993b99f68eb52456c
Aelloc <aelloc@singlebit.org>
2017-01-31 13:52:14 -0200

    refactor(Error): turn into a independent section

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <i class="fa fa-exclamation-triangle"></i> <b>{{page.status}} - {{page.message}}</b>
-    </div>
+    <section class="section">
+        <div class="container">
+            <div class="notification is-warning has-text-centered">
+                <i class="fa fa-exclamation-triangle"></i> <b>{{page.status}} - {{page.message}}</b>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
     import { mapState } from 'vuex';
