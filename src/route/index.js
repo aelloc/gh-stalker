91b051971a8fb46335d20925b164437ea901afc4
Aelloc <aelloc@singlebit.org>
2017-01-31 14:46:14 -0200

    refactor(route): remove '/' route

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import App from '../components/App.vue';
 import Profile from '../components/Profile.vue';
 
 Vue.use(VueRouter);
@@ -9,7 +8,6 @@ Vue.use(VueRouter);
 export default new VueRouter({
     base: '/',
     routes: [
-        { path: '/', component: App },
         { path: '/profile/:user', component: Profile }
     ]
 });
