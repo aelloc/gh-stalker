d6992de9045f3da1dfcaaa4df1ea674efa25aa37
Aelloc <aelloc@singlebit.org>
2017-01-31 14:41:52 -0200

    refactor(Hero): remove search component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,22 +1,25 @@
 <template>
-    <section class="hero is-dark">
+    <section class="hero is-dark is-medium">
         <div class="hero-head">
             <header class="nav">
                 <div class="container">
                     <div class="nav-left">
-                        <a class="nav-item" target="_blank" href="http://github.com">
+                        <a class="nav-item" href="/">
                             <img src="../assets/images/logo.png">
+                        </a>
+                    </div>
+                    <div class="nav-center">
+                        <a class="nav-item is-active">GH Stalker</a>
+                    </div>
+                    <div class="nav-right">
+                        <a class="nav-item" target="_blank" href="//github.com">
+                            <span class="icon">
+                                <i class="fa fa-github"></i>
+                            </span>
                         </a>
                     </div>
                 </div>
             </header>
-        </div>
-
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <h1 class="title is-1 modeka">gh stalker</h1>
-                <search></search>
-            </div>
         </div>
     </section>
 </template>
