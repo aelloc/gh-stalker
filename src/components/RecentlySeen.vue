5af715f63d58d0148f4decc6aec7905b4ad918ec
Aelloc <aelloc@singlebit.org>
2017-02-01 17:50:48 -0200

    feat(RecentlySeen): add new component to show the latest visited profiles

---
+++ src/components/RecentlySeen.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="mostViewed.length > 0">
+        <users title="Recently seen" :users="mostViewed"></users>
+    </div>
+</template>
+<script>
+    import { keys, local } from '../services/storageService';
+    import Users from './Users.vue';
+
+    export default {
+        components: { Users },
+        data() {
+            return {
+                mostViewed: local.get(keys.MOST_VIEWED) || []
+            }
+        }
+    }
+</script>
+<style>
+</style>
