e8e12a04bdfe2c9e916ac912293f6d1bca523015
Aelloc <aelloc@singlebit.org>
2017-02-01 17:53:35 -0200

    feat(router): in the '/' path show the RecentlySeen component
    #17

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router';
 
 import Profile from '../components/Profile.vue';
 import Search from '../components/Search.vue';
+import RecentlySeen from '../components/RecentlySeen.vue';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     base: '/',
     routes: [
+        { path: '/', component: RecentlySeen },
         { path: '/search', component: Search },
         { path: '/profile/:username', component: Profile, props: true }
     ]
