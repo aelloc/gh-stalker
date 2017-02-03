b6a399dc7f5922fa4f829fb48c952e6dbccacb88
Aelloc <aelloc@singlebit.org>
2017-02-03 16:25:04 -0200

    feat(Toggle): add component of toggle button

---
+++ src/components/Toggle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>{{title}}</p>
+        <button @click="change" class="button" :class="{'is-primary': enabled, 'is-danger': !enabled}">
+            {{ enabled ? 'Enabled' : 'Disabled' }}
+        </button>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['title', 'default'],
+        data() {
+            return {
+                enabled: this.default
+            };
+        },
+        methods: {
+            change() {
+                this.$data.enabled = !this.$data.enabled;
+                this.$emit('change', this.$data.enabled);
+            }
+        }
+    }
+</script>
+<style scoped>
+    p {
+        display: inline-block;
+        line-height: 28px;
+    }
+</style>
