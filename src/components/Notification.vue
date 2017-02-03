53bc570ccf55ca410f13385fafff390e3c6a9ba2
Aelloc <aelloc@singlebit.org>
2017-02-03 16:24:06 -0200

    feat(Notification): add component to show notifications

---
+++ src/components/Notification.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-show="show" class="notification" :class="type">
+        <button @click="close" class="delete"></button>
+        {{ message }}
+    </div>
+</template>
+<script>
+    export default {
+        props: ['message', 'type', 'show'],
+        methods: {
+            close() {
+                this.show = false;
+            }
+        }
+    }
+</script>
+<style></style>
