630079ecf82ac7211b9eed4a72da5fbb94738e82
Aelloc <aelloc@singlebit.org>
2017-02-06 18:51:04 -0200

    feat(GitHubService): add page parameter to the searchUser method

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -5,7 +5,7 @@ const default_unk_avatar = 'https://i2.wp.com/assets-cdn.github.com/images/grava
 export const branchs = (owner = '', repository = '') => Https.get(`/repos/${owner}/${repository}/branches`);
 export const commits = (owner = '', repository = '') => Https.get(`/repos/${owner}/${repository}/commits`);
 export const commitsAt = (owner = '', repository = '', branch = '') => Https.get(`/repos/${owner}/${repository}/commits?sha=${branch}`);
-export const searchUser = (user = '') => Https.get(`/search/users?utf8=%E2%9C%93&q=${user.replace(" ", "+")}`);
+export const searchUser = (user = '', page = 1) => Https.get(`/search/users?utf8=%E2%9C%93&page=${page}&q=${user.replace(" ", "+")}`);
 export const searchRepos = (user = '') => Https.get(`/users/${user}/repos`);
 export const user = (user = '') => Https.get(`/users/${user}`);
 export const commitComments = (owner = '', repo = '', ref = '') => Https.get(`/repos/${owner}/${repo}/commits/${ref}/comments`);
