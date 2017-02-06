095426097cb36d316f67b6e71c42dd6737e85fcd
Aelloc <aelloc@singlebit.org>
2017-02-06 18:48:54 -0200

    refactor(Hero): change the order of showing options in the navbar

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -18,16 +18,16 @@
                                     <i class="fa fa-search"></i>
                                 </span>
                             </router-link>
-                            <a class="nav-item" target="_blank" href="//github.com">
-                                <span class="icon">
-                                    <i class="fa fa-github"></i>
-                                </span>
-                            </a>
                             <router-link to="/settings" class="nav-item">
                                 <span class="icon">
                                     <i class="fa fa-cog"></i>
                                 </span>
                             </router-link>
+                            <a class="nav-item" target="_blank" href="//github.com">
+                                <span class="icon">
+                                    <i class="fa fa-github"></i>
+                                </span>
+                            </a>
                         </div>
                     </div>
                 </nav>
