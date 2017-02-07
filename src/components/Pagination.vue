dc8e75b727dbec947d043f298606de1eec7e4cb2
Aelloc <aelloc@singlebit.org>
2017-02-07 14:37:30 -0200

    feat(Pagination): add pagination component controls

---
+++ src/components/Pagination.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="pagination is-centered">
+        <a class="pagination-previous" @click="changePage" :class="{'is-disabled': actual_page == first}">Previous</a>
+        <a class="pagination-next" @click="changePage" :class="{'is-disabled': actual_page == last}">Next</a>
+        <ul class="pagination-list">
+            <li>
+                <a class="pagination-link is-current">{{ actual_page }}</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script>
+    export default {
+        props: {
+            first: { type: Number, default: 1 }, 
+            last: { type: Number, default: 1 }
+        },
+        data() {
+            return {
+                actual_page: 1
+            };
+        },
+        methods: {
+            changePage(event) {
+                let type = event.target.innerHTML;
+                type === 'Next' ? this.actual_page++ : this.actual_page--;
+                this.$emit('changePage', { type, actual_page: this.actual_page });
+            }
+        }
+    }
+</script>
+<style></style>
