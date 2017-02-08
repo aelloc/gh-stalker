090471deb8b0c4192c65046ad3debb1f19aefe8f
Aelloc <aelloc@singlebit.org>
2017-02-08 15:15:52 -0200

    feat(Footnote): add behavior to change class when the page has content

--- src/components/Footnote.vue
+++ src/components/Footnote.vue
@@ -1,12 +1,13 @@
 <template>
-    <footer class="footer">
+    <footer class="footer" :class="{'at-bottom': !hasContent, 'sticky': hasContent}">
         <div class="container">
             <div class="content has-text-centered">
                 <p>
                     <strong>GH Stalker</strong> created by <a href="//github.com/calvingerling" target="_blank">Calvin Gerling</a>.
                 </p>
                 <p>
-                    Made with <a href="//vuejs.org" target="_blank">Vue.js</a>, <a href="//bulma.io" target="_blank">Bulma</a>, <a href="//webpack.github.io" target="_blank">Webpack</a> and the <a href="//developer.github.com/" target="_blank">GitHub API</a>.
+                    Made with <a href="//vuejs.org" target="_blank">Vue.js</a>, <a href="//bulma.io" target="_blank">Bulma</a>,
+                    <a href="//webpack.github.io" target="_blank">Webpack</a> and the <a href="//developer.github.com/" target="_blank">GitHub API</a>.
                 </p>
                 <p>
                     <a class="icon" href="//github.com/calvingerling/gh-stalker">
@@ -17,12 +18,36 @@
         </div>
     </footer>
 </template>
-<script></script>
+<script>
+    export default {
+        data() {
+            return {
+                hasContent: this.$route.path !== '/'
+            };
+        },
+        watch: {
+            $route: {
+                immediate: true,
+                handler(val, oldVal) {
+                    this.hasContent = val.path !== '/';
+                }
+            }
+        }
+    }
+</script>
 <style scoped>
     footer {
-        position: absolute;
-        right: 0;
+        bottom: 0;
         left: 0;
         padding: 1rem;
+        text-align: center;
+        width: 100%;
+    }
+    .at-bottom {
+        position: absolute;
+    }
+
+    .sticky {
+        position: relative;
     }
 </style>
