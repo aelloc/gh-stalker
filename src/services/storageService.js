dcb2686adadfdc81fef8abe0428d86b1c607eebe
Aelloc <aelloc@singlebit.org>
2017-02-25 21:41:47 -0300

    refactor(storageService): remove function to add in array

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -2,19 +2,6 @@ export const local = {
     get(key) {
         return JSON.parse(localStorage.getItem(key));
     },
-    add(key, value) {
-        let storageValue = this.get(key);
-        if (!storageValue) {
-            this.set(key, JSON.stringify([]));
-            storageValue = [];
-        }
-
-        let index = storageValue.findIndex(e => e.login == value.login);
-        if (index == -1) storageValue.push({ ...value, lastView: new Date()});
-        else storageValue[index].lastView = new Date();
-
-        this.set(key, storageValue);
-    },
     set(key, value) {
         localStorage.setItem(key, JSON.stringify(value));
     },
