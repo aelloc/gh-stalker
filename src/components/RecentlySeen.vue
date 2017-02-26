615a0de612b81499aa2b25f7a7ef9782c5051000
Aelloc <aelloc@singlebit.org>
2017-02-25 21:42:38 -0300

    refactor(RecentlySeen): use store to show the users who have been recently seen

--- src/components/RecentlySeen.vue
+++ src/components/RecentlySeen.vue
@@ -10,13 +10,9 @@
     export default {
         components: { Users },
         data() {
-            let mostViewed = local.get(keys.MOST_VIEWED) || [], show = local.get(keys.SETTINGS);
-            mostViewed = mostViewed.sort((a, b) => new Date(b.lastView) - new Date(a.lastView));
-            show = show !== null ? show.recentlySeen : true;
-
             return {
-                mostViewed,
-                show
+                mostViewed: this.$store.state.history.users,
+                show: this.$store.state.settings.recentlySeen
             }
         }
     }
